1e451c198505b6a83f862187a023e5c66a4a2e3f
Chau Tran <cctran96@gmail.com>
2021-06-10 19:55:21 -0700

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+
+adele = Artist.create(name: "Adele")
+miley = Artist.create(name: "Miley Cyrus")
+
+Song.create(title: "Hello", artist: adele)
+Song.create(title: "Chasing Pavements", artist: adele)
+Song.create(title: "Rolling in the Deep", artist: adele)
+Song.create(title: "Wrecking Ball", artist: miley)
+Song.create(title: "The Climb", artist: miley)
